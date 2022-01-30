1e8133593da847569f1a1e72d9207660710bb2e1
xjose97x <joseignacioescudero@gmail.com>
2022-01-29 19:52:34 -0500

    schema validation and readme comments

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ The challenge is to implement a limited-scope OpenAPI schema validator.
 - Write code in [src/index](./src/index.ts) that takes in a `JSONObject` schema and a JSON object that returns true or false depending on whether or not it matches.
 - The challenge should only take 2-4 hours - so no need to over-engineer a solution, although make sure the implementation is readable
 - Write additional test-cases in [user-test-cases.ts](./src/tests/cases/user-test-cases.ts) to add additional tests not covered (this is optional if you have extra time)
+
+
+# Commnents
+- Test case #9 (`Invalidates mismatch nested array`) fails but it has a valid schema.
+- Test case #11 (`Invalidate cart with bad enums`) passes but it has an invalid schema. The property `size` has as value `XL`, however the only valid values for `size` are `'xs', 's', 'm', 'l'`.
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,51 @@
 import { JSONObject } from './types';
 
-export function validateSchema(data: any, schema: JSONObject): boolean {
+function validateType(type: string, data: any): boolean {
+  switch (type) {
+    case 'object':
+      return typeof data === 'object' && !Array.isArray(data);
+    case 'array':
+      return Array.isArray(data);
+    case 'string':
+    case 'boolean':
+    case 'number':
+      return typeof data === type;
+  }
   return false;
+}
+
+function validateObjectKeys(objectKeys: string[], requiredKeys: string[], validKeys: string[]): boolean {
+  const hasRequiredKeys = requiredKeys.every(key => objectKeys.includes(key));
+  if (!hasRequiredKeys) {
+    return false;
+  }
+  const hasOnlyKnownKeys = objectKeys.every(key => validKeys.includes(key));
+  if (!hasOnlyKnownKeys) {
+    return false;
+  }
+  return true;
+}
+
+export function validateSchema(data: any, schema: JSONObject): boolean {
+  if (!validateType(schema.type, data)) {
+    return false;
+  }
+
+  if (schema.type === 'string' && schema.enum) {
+    return schema.enum.includes(data);
+  }
+  else if (schema.type === 'array') {
+    return data.every(value => validateSchema(value, schema.items));
+  }
+  else if (schema.type === 'object') {
+    if (!validateObjectKeys(Object.keys(data), schema.required, Object.keys(schema.properties))) {
+      return false;
+    }
+    const areKeysValid = Object.entries(data).every(([key, value]) => validateSchema(value, schema.properties[key]))
+    if (!areKeysValid) {
+      return false;
+    }
+  }
+
+  return true;
 }
